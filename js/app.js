31360fa5005b1051df4883ccaeafcc31e36283a3
KareemEldeen2416 <120192930+KareemEldeen2416@users.noreply.github.com>
2023-05-02 09:58:22 +0300

    Fianl commit

--- js/app.js
+++ js/app.js
@@ -18,6 +18,10 @@ let finalDay = 0;
 let finalMonth = 0 ; 
 let finalYear =0;
 
+let valid1 = false;
+let valid2 = false;
+let valid3 = false;
+
 
     let dayNumber =0 ;
     let monthNumber = inputList[1].value;
@@ -27,22 +31,28 @@ conversionButton.addEventListener('click' , ()=>{
     
     if(inputList[0].value > 31 || inputList[0].value < 1){
         validList[0].classList.add("Invalid");
+        valid1 = false;
     }else{
          dayNumber = inputList[0].value;
          validList[0].classList.remove("Invalid");
+         valid1= true;
     }
 
 
     if(inputList[1].value < 1 || inputList[1].value > 12){
         validList[1].classList.add("Invalid");
+        valid2 =false;
     }else{
+        valid2 =true;
         monthNumber = inputList[1].value;
         validList[1].classList.remove("Invalid");
     }
 
     if(inputList[2].value < 1 || inputList[2].value >year){
         validList[2].classList.add("Invalid");
+        valid3 =false;
     }else{
+        valid3 = true;
         yearNumber =inputList[2].value;
         validList[2].classList.remove("Invalid");
     }
@@ -50,9 +60,7 @@ conversionButton.addEventListener('click' , ()=>{
 
 
 
-    console.log(dayNumber);
-    console.log(monthNumber);
-    console.log(yearNumber);
+    if ( valid1 && valid2 && valid3){
 
     if(month < monthNumber){
         finalYear = year - yearNumber -1;
@@ -80,4 +88,6 @@ conversionButton.addEventListener('click' , ()=>{
     labelsList[0].innerText = finalYear;
     labelsList[1].innerText = finalMonth;
     labelsList[2].innerText = finalDay;
+
+   }
 });
